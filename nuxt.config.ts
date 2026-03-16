885408627ad315a25a1387d547fb34e101b5c52f
shunta.ishiduka <uuphoto@gmail.com>
2026-03-16 19:57:54 +0000

    add: HMR安定の為の設定修正

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,12 +2,4 @@
 export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
   devtools: { enabled: true },
-  vite: {
-    server: {
-      watch: {
-        // Docker/devcontainer環境でのHMR安定化
-        usePolling: true
-      }
-    }
-  }
 })
