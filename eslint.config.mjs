0e27c637c3956e8b967e4408a8a00f9e52492417
shunta.ishiduka <uuphoto@gmail.com>
2026-03-25 09:44:33 +0000

    add: eslintとprettierを追加

---
+++ eslint.config.mjs
@@ -0,0 +1,13 @@
+// @ts-check
+import withNuxt from './.nuxt/eslint.config.mjs'
+
+export default withNuxt({
+  rules: {
+    'vue/block-lang': [
+      'error',
+      {
+        script: { lang: 'ts' }
+      }
+    ]
+  }
+})
