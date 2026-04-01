2c320e34283a62970d909edbaca6ceaabc32dab5
shunta.ishiduka <uuphoto@gmail.com>
2026-04-01 08:09:23 +0000

    fix: eslintがtsの構文をパースできていなかったため再度修正

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,6 +1,15 @@
 import withNuxt from './.nuxt/eslint.config.mjs'
+import vueParser from 'vue-eslint-parser'
+import tsParser from '@typescript-eslint/parser'
 
 export default withNuxt({
+  files: ['**/*.vue'],
+  languageOptions: {
+    parser: vueParser,
+    parserOptions: {
+      parser: tsParser
+    }
+  },
   rules: {
     'vue/block-lang': [
       'error',
