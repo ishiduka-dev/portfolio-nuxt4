08cb7c18922f257f390a3deb98c59c513d0c8a7b
shunta.ishiduka <uuphoto@gmail.com>
2026-04-05 19:38:02 +0000

    add: eslintの対象拡張子を調整

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -2,28 +2,30 @@ import withNuxt from './.nuxt/eslint.config.mjs'
 import vueParser from 'vue-eslint-parser'
 import tsParser from '@typescript-eslint/parser'
 
-export default withNuxt({
-  files: ['**/*.vue'],
-  languageOptions: {
-    parser: vueParser,
-    parserOptions: {
-      parser: tsParser
-    }
-  },
-  rules: {
-    'vue/block-lang': [
-      'error',
-      {
-        script: { lang: 'ts' }
+export default withNuxt([
+  {
+    files: ["**/*.{js,ts,vue}"],
+    languageOptions: {
+      parser: vueParser,
+      parserOptions: {
+        parser: tsParser
       }
-    ],
-    'vue/html-self-closing': [
-      'error',
-      {
-        html: {
-          void: 'always'
+    },
+    rules: {
+      'vue/block-lang': [
+        'error',
+        {
+          script: { lang: 'ts' }
         }
-      }
-    ]
+      ],
+      'vue/html-self-closing': [
+        'error',
+        {
+          html: {
+            void: 'always'
+          }
+        }
+      ]
+    }
   }
-})
+])
