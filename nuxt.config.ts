5e06eb3dd8a0c0fd7143edd7ba85bfe26ecac360
shunta.ishiduka <uuphoto@gmail.com>
2026-04-06 17:23:37 +0000

    add: 外部端末から確認できるようにdocker構成を調整

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,8 +2,11 @@
 export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
   devtools: { enabled: true },
+  devServer: {
+    host: '0.0.0.0',
+    port: 3000
+  },
   modules: ['@nuxt/eslint'],
-
   css: ['~/assets/css/theme.css', '~/assets/scss/main.scss'],
   vite: {
     css: {
